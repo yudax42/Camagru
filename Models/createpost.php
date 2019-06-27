1edc29a5237ca01c9f1c7de9c423ead68c54ac5e
Youssef Benadda <ybenadda@e1r12p4.1337.ma>
2019-06-27 20:40:02 +0100

    merge

--- Models/createpost.php
+++ Models/createpost.php
@@ -1,14 +1,20 @@
 <?php
     session_start();
-    if(!empty($_POST['hidden_data']))
+    if(!empty($_POST['hidden_data']) && !empty($_POST['img']))
     {
         $upload_dir = "upload/";
         $img = $_POST['hidden_data'];
         $img = str_replace('data:image/png;base64,', '', $img);
         $img = str_replace(' ', '+', $img);
         $data = base64_decode($img);
-        $file = $upload_dir . mktime() . ".png";
-        $success = file_put_contents($file, $data);
+        $file = $upload_dir . mktime() .$_POST['img']. ".png";
+        if($_POST['img'] == 'hacker')
+        {
+            $src = imagecreatefrompng('../Assets/obj/hacker.png');
+            imagecopymerge($data, $src, 10, 9, 0, 0, 181, 180, 100);
+            $success = file_put_contents($file, $data);
+        }
+       
     }
-    file_put_contents("name", $_POST['img']);
+    
 ?>
