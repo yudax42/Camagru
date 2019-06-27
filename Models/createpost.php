ebb02c4c342db9c88df127f33365e688c6ae7e67
Youssef Benadda <ybenadda@e1r12p4.1337.ma>
2019-06-27 20:26:48 +0100

    start merge

--- Models/createpost.php
+++ Models/createpost.php
@@ -1,11 +1,14 @@
 <?php
     session_start();
-    require_once "../config/connect.php";
-    $upload_dir = "upload/";
-    $img = $_POST['hidden_data'];
-    $img = str_replace('data:image/png;base64,', '', $img);
-    $img = str_replace(' ', '+', $img);
-    $data = base64_decode($img);
-    $file = $upload_dir . mktime() . ".png";
-    $success = file_put_contents($file, $data);
-    print $success ? $file : 'Unable to save the file.';
+    if(!empty($_POST['hidden_data']))
+    {
+        $upload_dir = "upload/";
+        $img = $_POST['hidden_data'];
+        $img = str_replace('data:image/png;base64,', '', $img);
+        $img = str_replace(' ', '+', $img);
+        $data = base64_decode($img);
+        $file = $upload_dir . mktime() . ".png";
+        $success = file_put_contents($file, $data);
+    }
+    file_put_contents("name", $_POST['img']);
+?>
---
+++ Scripts/cam.js
@@ -0,0 +1,55 @@
+var video = document.getElementById('video');
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480);
+});
+
+
+if(document.getElementById('obj').checked) {
+    document.getElementById("snap").disabled = false;
+}
+else
+    document.getElementById("snap").disabled = true;
+function enable()
+{
+    document.getElementById("snap").disabled = false;
+}
+
+function uploadEx() {
+    if(document.getElementById('obj').checked) {
+        var dataURL = canvas.toDataURL("image/png");
+        document.getElementById('img').value = document.getElementById('obj').value;
+        console.log(document.getElementById('img').value);
+        document.getElementById('hidden_data').value = dataURL;
+        var fd = new FormData(document.forms["form1"]);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '../Models/createpost.php', true);
+
+        xhr.upload.onprogress = function(e) {
+            if (e.lengthComputable) {
+                var percentComplete = (e.loaded / e.total) * 100;
+                console.log(percentComplete + '% uploaded');
+                alert('Succesfully uploaded');
+            }
+        };
+
+        xhr.onload = function() {
+
+        };
+        xhr.send(fd);
+    }
+};
