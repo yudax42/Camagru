b194877c5d917b0f11ec2fc393e1f6681c68d6ee
Youssef Benadda <ybenadda@e1r12p4.1337.ma>
2019-06-28 21:05:12 +0100

    style bar

--- Views/logged.view.php
+++ Views/logged.view.php
@@ -6,46 +6,52 @@
 ?>
     <body class="animated fadeIn">
         <h1>Welcome, <?php echo $_SESSION["username"];?></h1>
-       
+       <div class="container">
         <?php
             $list_post = new Pagination(5,"posts");
-            if($list_post->paginate())
+            if(isset($_GET['page']) || !isset($_GET['page']))
             {
+                if($list_post->paginate())
+                {
 
-                foreach($list_post->fetch as $row)
-                {
-                    echo "<div class=post>";
-                        echo "<section class=title>";
-                            echo "<div class=name>".$row['username']."</div>";
-                            echo "<div class=creationdate>".$row['creation_date']."</div>";
-                        echo "</section>";
-                        echo "<img src='../Models/upload/".$row['image']."'/>";
-                        echo "<section class=likes> likes " . $row['likes'] ."</section>";
-                        echo "<form action='#' method='POST'>
-                        <section class=comment>". 
-                        "<input type='text' name='comment'>"
-                        ."<input type='hidden' name='id' value='".$row['id']
-                        ."'/><button type='submit'>submit</button></section>
-                        
-                        </form>";
-                        if($post->fetchcomments("comments",$row["id"]))
-                        {
-                            foreach($post->fetch as $com)
+                    foreach($list_post->fetch as $row)
+                    {
+                        echo "<div class=post>";
+                            echo "<section class=title>";
+                                echo "<div class=name>".$row['username']."</div>";
+                                echo "<div class=creationdate>".$row['creation_date']."</div>";
+                            echo "</section>";
+                            echo "<img src='../Models/upload/".$row['image']."'/>";
+                            echo "<section class=likes> likes " . $row['likes'] ."</section>";
+                            echo "<form action='#' method='POST'>
+                            <section class=comment>". 
+                            "<input type='text' name='comment'>"
+                            ."<input type='hidden' name='id' value='".$row['id']
+                            ."'/><button type='submit'>submit</button></section>
+                            
+                            </form>";
+                            if($post->fetchcomments("comments",$row["id"]))
                             {
-                                echo $com["username"] . "=> " . $com["comment"]. "<br>";
+                                foreach($post->fetch as $com)
+                                {
+                                    echo $com["username"] . "=> " . $com["comment"]. "<br>";
+                                }
                             }
-                        }
-                        
+                            
+                        echo "</div>";
+                    }
+                    $page = 1;
+                    echo "<div class='conpage'>";
+                    while($page <= $list_post->total_pages)
+                    {
+                        echo '<a class=pagination href="?page=' . $page . '">' . $page . '</a>';
+                        $page++;
+                    }
                     echo "</div>";
-                }
-                $page = 1;
-                while($page < $list_post->total_pages)
-                {
-                    echo '<a href="#?page=' . $page . '">' . $page . '</a>';
-                    $page++;
                 }
             }
         ?>
+        </div>
         </div>
     </body>
 </html>
